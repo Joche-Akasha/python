09d1a34e2b29028bbfa66228b12c3fd6d9e84644
Akasha <Aka@example.com>
2018-02-14 16:34:33 +0100

    Segundo Commit

---
+++ Ejer2.py
@@ -0,0 +1,15 @@
+# si se usa ra_input seria a=int(raw_input('Primer Numero'))
+
+x=input('Pon un Numero entre 1-100   ')
+y=input('Pon otro Numero entre 1-100 menor al primero   ')
+print
+
+if y%2 == 0:
+ while (x>y):
+	print y
+	y=y+2
+else:
+ while (x>y):
+	print y+1
+	y=y+2
+ 
---
+++ prueba.py
@@ -0,0 +1,5 @@
+﻿x=input('Pon el ano en curso')
+y=input('Año de Naciemiento')
+print
+aux = x - y
+print 'La edad es: ' +str(aux)
